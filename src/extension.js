6740061576c66a951adab1b21b2958a0d53b1ec6
Tyler Hughes <tyler@tylerhughes.info>
2018-03-29 01:12:59 -0500

    Moved code to the src folder and aded functions for when the user clicks in the context menu items

--- src/extension.js
+++ src/extension.js
@@ -8,18 +8,24 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(vscode.commands.registerCommand('imageOptimizer.losslessOptimization', losslessOptimization));
+    context.subscriptions.push(vscode.commands.registerCommand('imageOptimizer.lossyOptimization', lossyOptimization));
 }
+
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
 function deactivate() {
+    
 }
+
 exports.deactivate = deactivate;
+
+function losslessOptimization() {
+    console.log('Entered losslessOptimization function.');
+}
+
+function lossyOptimization() {
+    console.log('Entered lossyOptimization function');
+}
